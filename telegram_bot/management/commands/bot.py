a04a979230bb3933112377f081425f88eb93c387
Nikita <NikitaMineev2020@yandex.ru>
2024-05-10 11:12:58 +0800

    Бот поверхностно готов

--- telegram_bot/management/commands/bot.py
+++ telegram_bot/management/commands/bot.py
@@ -31,17 +31,15 @@ class Command(BaseCommand):
 
         @bot.message_handler(commands=['enter'])
         def enter_user(message):
-            print(message.from_user.id)
-            role = Owner.objects.get(telegram_id=message.chat.id).role
-            print(role)
-            if role in 'AO':
-                print('AO')
+            check, role = check_role(message)
+            if check:
                 markup = types.ReplyKeyboardMarkup()
                 if role == 'O':
                     btn1 = types.KeyboardButton('/add_administrator')
                     btn2 = types.KeyboardButton('/orders')
                     btn3 = types.KeyboardButton('/work')
-                    markup.add(btn1, btn2, btn3)
+                    btn4 = types.KeyboardButton('/delete_administrator')
+                    markup.add(btn1, btn4, btn2, btn3)
                 else:
                     btn2 = types.KeyboardButton('/orders')
                     btn3 = types.KeyboardButton('/work')
@@ -50,13 +48,15 @@ class Command(BaseCommand):
 
         @bot.message_handler(commands=['orders'])
         def send_message(message):
-            if check_role(message):
+            _, check = check_role(message)
+            if check:
                 bot.send_message(message.from_user.id,
                                  f'Количество необработанных заявок: {SellerApplication.objects.all().count()}')
 
         @bot.message_handler(commands=['work'])
         def send_work(message):
-            if check_role(message):
+            _, check = check_role(message)
+            if check:
                 markup = types.ReplyKeyboardMarkup()
                 msg, first_application = format_message(message)
                 if not first_application == '...':
@@ -70,7 +70,61 @@ class Command(BaseCommand):
                     markup.add(btn1)
                     bot.send_message(message.from_user.id, 'Ждем заявки', reply_markup=markup)
 
+        @bot.message_handler(commands=['add_administrator'])
+        def add_administrator(message):
+            check, role = check_role(message)
+            if role == 'O':
+                bot.send_message(message.from_user.id,
+                                 'Введите через пробел данные об новом администраторе\ntelegram_id first_name last_name role(A,O)\n ВАЖНО: ПОЛЯ ПРОПУСКАТЬ НЕЛЬЗЯ!')
+                bot.register_next_step_handler(message, add_admin)
+
+        @bot.message_handler(commands=['delete_administrator'])
+        def delete_administrator(message):
+            check, role = check_role(message)
+            if role == 'O':
+                if Owner.objects.filter(role='A').count() != 0:
+                    bot.send_message(message.from_user.id,
+                                     format_administrator_list(message) + '\n\n Введите порядковый номер')
+                    bot.register_next_step_handler(message, del_admin)
+                else:
+                    bot.send_message(message.from_user.id, 'Администраторов лишних нет :(')
+
+        def del_admin(message):
+            markup = types.ReplyKeyboardMarkup()
+            btn1 = types.KeyboardButton('/enter')
+            markup.add(btn1)
+
+            try:
+                if Owner.objects.filter(pk=int(message.text)):
+                    bot.send_message(message.from_user.id,
+                                     f'Администратор под номером {int(message.text)} успешно удален',
+                                     reply_markup=markup)
+                    Owner.objects.filter(pk=int(message.text)).delete()
+            except Exception:
+                bot.send_message(message.from_user.id,
+                                 'Вы допустили ошибку в номере администратора :(\n Попробуйте снова')
+                bot.register_next_step_handler(message, del_admin)
+
+        def add_admin(message):
+            data = message.text.split()
+            if len(data) == 4:
+                Owner.objects.create(telegram_id=data[0],
+                                     first_name=data[1],
+                                     last_name=data[2],
+                                     role=data[3])
+                markup = types.ReplyKeyboardMarkup()
+                btn1 = types.KeyboardButton('/enter')
+                markup.add(btn1)
+                bot.send_message(message.from_user.id, 'Готово!', reply_markup=markup)
+            else:
+                bot.send_message(message.from_user.id, 'Вы допустили ошибку')
+
         def work_application(message):
+            """Работа над заявками"""
+            markup = types.ReplyKeyboardMarkup()
+            btn1 = types.KeyboardButton('/enter')
+            btn2 = types.KeyboardButton('/work')
+            markup.add(btn1, btn2)
             if message.text.lower() == 'одобрить':
                 application = SellerApplication.objects.first()
                 CustomUser.objects.create(
@@ -84,16 +138,27 @@ class Command(BaseCommand):
                     username=application.name
                 )
                 SellerApplication.objects.first().delete()
-                bot.send_message(message.from_user.id, 'Заявка одобрена..')
+
+                bot.send_message(message.from_user.id, 'Заявка одобрена..', reply_markup=markup)
+            elif message.text.lower() == 'отклонить':
+                msg, _ = format_message(message)
+                bot.send_message(message.from_user.id,
+                                 msg + ' \n\nОТКЛОНЕНА и УДАЛЕНА ИЗ БАЗЫ ДАННЫХ!',
+                                 reply_markup=markup)
+                SellerApplication.objects.first().delete()
 
         def check_role(message):
+            """Проверка ролей администраторов"""
             role = Owner.objects.get(telegram_id=message.chat.id).role
             if role in 'AO':
-                return True
+                if role == 'O':
+                    return True, 'O'
+                return True, 'A'
             else:
                 return False
 
         def format_message(message):
+            """Форматирование сообщения заявки"""
             first_application = SellerApplication.objects.first()
             if first_application:
                 msg = (f'Фамилия: {first_application.last_name}\n'
@@ -107,5 +172,13 @@ class Command(BaseCommand):
                 return msg, first_application
             else:
                 return 'Заявок нету', '...'
+
+        def format_administrator_list(message):
+            owners = Owner.objects.filter(role='A').all()
+            msg = ''
+            for owner in owners:
+                msg += f'{owner.pk}: {owner.last_name}: {owner.first_name}\n'
+
+            return msg
 
         bot.polling(none_stop=True, interval=0)
